d8a1e358b8440208451339d61088958ed475219f
nyh <nsi3358@naver.com>
2024-01-03 21:00:50 +0900

    현재 선택된 메뉴 표시 구현

--- src/components/header/buttons/NavMenus.jsx
+++ src/components/header/buttons/NavMenus.jsx
@@ -2,16 +2,36 @@ import React, { useContext } from 'react';
 import CustomButton from './CustomButton';
 import styles from './buttons.module.css';
 import { CurrentViewContext } from '../../../context/CurrentViewContext';
+import { DarkModeContext } from '../../../context/DarkModeContext';
 
 export default function NavMenus() {
-  const { setCurrentView } = useContext(CurrentViewContext);
+  const { currentView, setCurrentView } = useContext(CurrentViewContext);
+  const { darkMode } = useContext(DarkModeContext);
+  const menuNames = {
+    All: 'All',
+    Action: 'Action',
+    Completed: 'Completed',
+  };
+
+  function checkCurrentMenu(menuName) {
+    const style =
+      currentView.current === menuName
+        ? darkMode
+          ? styles['curren-view-menu-darkMode']
+          : styles['curren-view-menu']
+        : null;
+
+    return style;
+  }
+
   return (
     <nav>
       <CustomButton
         style={styles['button-style']}
         clickEvent={() => setCurrentView(prev => ({ ...prev, current: 'All' }))}
       >
-        All
+        {menuNames.All}
+        <div className={checkCurrentMenu(menuNames.All)}></div>
       </CustomButton>
       <CustomButton
         style={styles['button-style']}
@@ -19,7 +39,8 @@ export default function NavMenus() {
           setCurrentView(prev => ({ ...prev, current: 'Action' }))
         }
       >
-        Action
+        {menuNames.Action}
+        <div className={checkCurrentMenu(menuNames.Action)}></div>
       </CustomButton>
       <CustomButton
         style={styles['button-style']}
@@ -27,7 +48,8 @@ export default function NavMenus() {
           setCurrentView(prev => ({ ...prev, current: 'Completed' }))
         }
       >
-        Completed
+        {menuNames.Completed}
+        <div className={checkCurrentMenu(menuNames.Completed)}></div>
       </CustomButton>
     </nav>
   );
